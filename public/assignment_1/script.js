8ef69adc117aee732e9a971edf06a034af367c10
bradleydayhoff <75460916+bradleydayhoff@users.noreply.github.com>
2021-03-05 12:02:31 -0500

    created assignment 1 file

---
+++ public/assignment_1/script.js
@@ -0,0 +1,33 @@
+async function windowActions() {
+    console.log('window loaded');
+    const form = document.querySelector('.userform');
+    const suggestions = document.querySelector('.suggestions');
+    const searchInput = document.querySelector('.search');
+    //   const targetList = document.querySelector('.target-list');
+  
+    const request = await fetch('/api');
+    const data = await request.json();
+  
+    function findMatches(wordToMatch, data) {
+      return data.filter((place) => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.name.match(regex);
+      });
+    }
+  
+    function displayMatches(event) {
+      const matchArray = findMatches(event.target.value, data);
+      const html = matchArray.map((place) => `
+            <li>
+              <span class="name>${place.name}, ${place.city}</span>
+            </li>
+          `).join('');
+      suggestions.innerHTML = html;
+    }
+    searchInput.addEventListener('keyup', (event) => {
+      event.preventDefault();
+      displayMatches(event);
+    });
+  }
+  
+  window.onload = windowActions;
